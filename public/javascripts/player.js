0ca1a5ffa6421ea14515b360c548d3b513f305eb
etervell <lwl1991@gmail.com>
2013-06-01 15:22:29 +0800

    Changed implementation
    Player body now uses array for linked list
    GameState implemented in snake.js

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -15,7 +15,7 @@ var DEBUG = false;
 //Player Class
 function Player(n, x, y){
     this.number = n;        //id/number of player
-    this.body = null;       //array of [x,y]//TODO: change this.
+    this.body = null;       //array of [x,y]
     this.alive = false;     //Boolean
     this.length = 5;        //int, length of snake.
     this.board_x = x;       //
@@ -28,9 +28,9 @@ Player.prototype.spawn = function(x,y){
     //Head at x,y, body length 5, tail x-5,y
     this.alive = true;
     this.movedir = 1;
-    this.body = new LinkedList();
+    this.body = []; ///new LinkedList();
     for(var i=0 ; i<this.length ; i++){
-        this.body.AddToTail([x-i,y]);
+        this.body.push([x-i,y]);
     }
     if (DEBUG) console.log("Player "+this.number+" spawned.");
 }
@@ -49,14 +49,14 @@ Player.prototype.nextmove = function(){
     //checking for collision? death?
         //Do in game engine.
     //return location of next move.
-    return [((this.board_x+(this.body.head.data[0]+DIR[this.movedir][0]))%this.board_x), ((this.board_y+(this.body.head.data[1]+DIR[this.movedir][1]))%this.board_y)];
+    return [((this.board_x+(this.body[0][0]+DIR[this.movedir][0]))%this.board_x), ((this.board_y+(this.body[0][1]+DIR[this.movedir][1]))%this.board_y)];
 }
 
 Player.prototype.removetail = function(){
-    this.body.removeAt(this.body.size-1);
+    this.body.pop();
 }
 Player.prototype.addhead = function(){
-    this.body.AddToHead(this.nextmove());
+    this.body.unshift(this.nextmove());
 }
 
 Player.prototype.blindmove = function(){
--- public/javascripts/snake.js
+++ public/javascripts/snake.js
@@ -14,7 +14,7 @@ socket.on("welcome", function (data) {
 //Main Game class.
 function Game(){
     
-    this.state = new GameState();
+    this.state = new GameState();   //Game State, the data to be transmitted to client
     
     this.state.num_players = 0;
     this.state.game_speed = 0;
@@ -42,24 +42,24 @@ Game.prototype.init = function(p_num_players){
 
 //Initialize Players
 Game.prototype.initplayers = function(){
-    this.player_array = new Array();
-    for(var i=0 ; i<this.numplayers ; i++){
-        this.player_array[i] = new Player(i,this.board_x,this.board_y);
-        this.player_array[i].spawn(5,3);
+    this.state.player_array = new Array();
+    for(var i=0 ; i<this.state.numplayers ; i++){
+        this.state.player_array[i] = new Player(i,this.state.board_x,this.state.board_y);
+        this.state.player_array[i].spawn(5,3);
     }
     
-    if (DEBUG) console.log("Player(s) Initialized : "+this.numplayers);
+    if (DEBUG) console.log("Player(s) Initialized : "+this.state.numplayers);
 }
 
 //Initialize Board parameters
 Game.prototype.init_board = function(x,y){
-    this.board_x = x;
-    this.board_y = y;
+    this.state.board_x = x;
+    this.state.board_y = y;
     this.board = new Array();
     
-    for(var i=0 ; i<this.board_y ; i++){
+    for(var i=0 ; i<this.state.board_y ; i++){
         this.board[i] = new Array();
-        for(var j=0 ; j<this.board_x ; j++){
+        for(var j=0 ; j<this.state.board_x ; j++){
             this.board[i][j] = 'O';
         }   
     }
@@ -70,8 +70,8 @@ Game.prototype.init_board = function(x,y){
 //Clear Board
 Game.prototype.clear_board = function(x,y){
 
-    for(var i=0 ; i<this.board_y ; i++){
-        for(var j=0 ; j<this.board_x ; j++){
+    for(var i=0 ; i<this.state.board_y ; i++){
+        for(var j=0 ; j<this.state.board_x ; j++){
             this.board[i][j] = 'O';
         }   
     }
@@ -85,9 +85,9 @@ Game.prototype.print_board = function(){
     
     var textboard="";
     
-    for(var i=0 ; i<this.board_y ; i++){
+    for(var i=0 ; i<this.state.board_y ; i++){
         var line = "";
-        for(var j=0 ; j<this.board_x ; j++){
+        for(var j=0 ; j<this.state.board_x ; j++){
             line+=this.board[i][j];
         }
         if (DEBUG) console.log(line);
@@ -114,19 +114,28 @@ Game.prototype.print_board = function(){
 Game.prototype.update_board = function(){ 
     if (DEBUG) console.log("Updating Board");
     
-    for(var i=0 ; i<this.player_array.length ; i++){
-        this.player_array[i].body.itr_reset();
+    /*
+    for(var i=0 ; i<this.state.player_array.length ; i++){
+        this.state.player_array[i].body.itr_reset();
         
-        while(this.player_array[i].body.itr_hasnext()){
+        while(this.state.player_array[i].body.itr_hasnext()){
             //Draw the body of the snake on the board
-            var temp = this.player_array[i].body.itr_next();
+            var temp = this.state.player_array[i].body.itr_next();
+            this.board[temp[1]][temp[0]] = 'S';
+        }
+    }*/
+    for(var i=0 ; i<this.state.player_array.length ; i++){
+       
+        for(var j=0 ; j< this.state.player_array[i].body.length ; j++){
+            //Draw the body of the snake on the board
+            var temp = this.state.player_array[i].body[j];
             this.board[temp[1]][temp[0]] = 'S';
         }
     }
 }
 
 Game.prototype.player_input = function(player, direction){
-    this.player_array[0].setDir(direction);
+    this.state.player_array[0].setDir(direction);
 }
 
 //On Ready
@@ -148,13 +157,13 @@ $(document).ready(function(){
     setkeydir();
     repeat();
     function setkeydir(){
-        game0.player_array[0].setDir(key_dir);
+        game0.state.player_array[0].setDir(key_dir);
         setTimeout(setkeydir, 200);
     }
 
     function repeat(){
         console.clear();
-        game0.player_array[0].blindmove();
+        game0.state.player_array[0].blindmove();
         game0.clear_board();
         game0.update_board();
         game0.print_board();
